032cf461c0f4bb13a8ab457384f148845ee4a91b
dharminder <144053986+DharminderPal@users.noreply.github.com>
2025-04-03 16:15:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,13 @@
 
-// import React from "react";
-// import './../styles/App.css';
-
-// const App = () => {
-//   return (
-//     <div>
-//         {/* Do not remove the main div */}
-//     </div>
-//   )
-// }
-
-// export default App
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ItemList from "./ItemList";
-import ItemDetail from "./ItemDetail";
-import "../../styles/App.css"; // Keeping your existing style import
+import './../styles/App.css';
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<ItemList />} />
-                <Route path="/items/:id" element={<ItemDetail />} />
-            </Routes>
-        </Router>
-    );
-};
+  return (
+    <div>
+        {/* Do not remove the main div */}
+    </div>
+  )
+}
 
-
-export default App;
+export default App
