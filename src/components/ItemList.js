85e5f9ad38f6d82bf030d3a66864e3f020820232
dharminder <144053986+DharminderPal@users.noreply.github.com>
2025-04-04 12:02:32 +0530

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,16 +1,24 @@
+import React from "react";
 import { Link } from 'react-router-dom';
 
-const ItemList = ({ items }) => (
-  <div>
-    <h2>Item List</h2>
-    <ul>
-      {items.map(item => (
-        <li key={item.id}>
-          <Link to={`/items/${item.id}`}>{item.name}</Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const ItemList = ({ items }) => {
+  return (
+    <div>
+      <h2>Item List</h2>
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        {items.map(item => (
+          <li key={item.id} style={{ margin: '10px 0' }}>
+            <Link 
+              to={`/items/${item.id}`} 
+              style={{ textDecoration: 'none', color: 'blue' }}
+            >
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ItemList;
